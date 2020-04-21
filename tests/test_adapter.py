06ec55254107987798d25565809e26e42ad7f6f5
John Vandenberg <jayvdb@gmail.com>
2020-04-21 13:47:26 +0700

    test_adapter: Use inheritance
    Reduce duplication of expected results, and better show
    which adapters are most similar, and the remaining tests
    better demonstrate how they are different.
    
    Improved some tests to be more specific.
    
    Also add tests for wiki.ros.org.
    
    Closes https://github.com/jayvdb/https-everywhere-py/issues/25

--- tests/test_adapter.py
+++ tests/test_adapter.py
@@ -19,6 +19,8 @@ from https_everywhere.adapter import (
     _HTTP_BLOCK_CODE,
 )
 
+from tests.test_upstream import TestUpstreamAdapter
+
 # Throughout _test_modwsgi_org refers to a scenario where the website
 # has been fixed, an a similar scenario has not yet been found
 # It may need to be mocked.
@@ -68,7 +70,7 @@ class TestRedirectBlockAdapter(unittest.TestCase):
         self.assertEqual(r.status_code, _HTTP_BLOCK_CODE)
 
 
-class TestEverywhereOnlyAdapter(unittest.TestCase):
+class TestEverywhereOnlyAdapter(TestUpstreamAdapter):
 
     cls = HTTPSEverywhereOnlyAdapter
 
@@ -83,76 +85,11 @@ class TestEverywhereOnlyAdapter(unittest.TestCase):
         original = r.history[0]
         self.assertEqual(original.url, url)
 
-    def test_example_com(self):
-        url = "http://example.com/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        r = s.get(url)
-        r.raise_for_status()
-        self.assertEqual(r.url, url)
-        self.assertEqual(r.history, [])
-
-    def test_webhostinggeeks_com_science(self):
+    def _test_webhostinggeeks_com_science(self):
         # https://github.com/EFForg/https-everywhere/issues/18867
         # https has wrong cert, which is a quick failure, but an exception exists
         url = "http://science.webhostinggeeks.com/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        r = s.get(url)
-        r.raise_for_status()
-        self.assertEqual(r.url, url)
-        self.assertEqual(r.history, [])
-
-    def test_fedmsg_com(self):
-        # https://github.com/EFForg/https-everywhere/issues/18867
-        # https redirects to http, but there are no rules to upgrade it to https
-        url = "http://fedmsg.com/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        r = s.get(url)
-        r.raise_for_status()
-        self.assertEqual(r.url, url)
-        self.assertEqual(r.history, [])
-
-    def test_shopzilla(self):
-        url = "http://www.shopzilla.com/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        r = s.get(url, timeout=5)
-        r.raise_for_status()
-        self.assertEqual(r.url, url)
-        self.assertEqual(r.history, [])
-
-    def test_whisper_sh(self):
-        url = "http://whisper.sh/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        r = s.get(url, timeout=5)
-        r.raise_for_status()
-        self.assertEqual(r.url, url)
-        self.assertEqual(r.history, [])
-
-    def test_thesyriacampaign(self):
-        url = "http://www.thesyriacampaign.org/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        r = s.get(url, timeout=5)
-        self.assertEqual(r.url, url)
-        self.assertEqual(r.status_code, 403)
-
-    def test_esncz_org(self):
-        url = "http://www.isc.vutbr.cz/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        r = s.get(url, timeout=5)
-        r.raise_for_status()
-        target = "https://www.esncz.org"
-        self.assertEqual(r.url, target)
-        self.assertEqual(len(r.history), 1)
-        original = r.history[0]
-        self.assertEqual(original.url, url)
-        self.assertEqual(original.status_code, 302)
-        self.assertEqual(original.reason, "Found")
+        # See super method
 
     def test_01_org(self):
         url = "http://01.org/"
@@ -180,23 +117,6 @@ class TestEverywhereOnlyAdapter(unittest.TestCase):
         self.assertEqual(original.status_code, 302)
         self.assertEqual(original.reason, _REASON)
         self.assertEqual(r.history[-1].url, "https://www.01.org/")
-
-    def test_medbank_mt(self):
-        url = "http://business.medbank.com.mt/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        r = s.get(url)
-        r.raise_for_status()
-        self.assertEqual(r.url, "https://www.medirect.com.mt")
-        # with self.assertRaises(requests.exceptions.ConnectionError):
-        #    s.get(url)
-
-    def test_my_vpnglobe(self):
-        url = "http://my.vpnglobe.com/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        with self.assertRaises(requests.exceptions.SSLError):
-            s.get(url)
 
     def test_python_org_packages(self):
         url = "http://packages.python.org/"
@@ -244,22 +164,7 @@ class TestChromePreloadAdapter(TestEverywhereOnlyAdapter):
         self.assertEqual(r.history[1].status_code, 301)
         self.assertEqual(r.history[1].reason, "Moved Permanently")
 
-    def test_python_org_packages(self):
-        url = "http://packages.python.org/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        r = s.get(url)
-        r.raise_for_status()
-        self.assertEqual(r.url, "https://pythonhosted.org/")
-        self.assertEqual(len(r.history), 2)
-        original = r.history[0]
-        self.assertEqual(original.url, url)
-        # 301 here vs 302
-        self.assertEqual(original.status_code, 301)
-        self.assertEqual(original.reason, "Moved Permanently")
-        self.assertEqual(r.history[1].url, url.replace("http://", "https://"))
-        self.assertEqual(r.history[1].status_code, 301)
-        self.assertEqual(r.history[1].reason, "Moved Permanently")
+    test_python_org_packages = TestUpstreamAdapter.test_python_org_packages
 
 
 class TestMozillaPreloadAdapter(TestChromePreloadAdapter):
@@ -277,12 +182,12 @@ class TestEverywhereAdapter(TestChromePreloadAdapter):
     test_python_org_packages = TestEverywhereOnlyAdapter.test_python_org_packages
 
 
-class TestForceAdapter(unittest.TestCase):
+class TestForceAdapter(TestEverywhereAdapter):
 
     cls = ForceHTTPSAdapter
 
-    def test_freerangekitten_com(self):
-        url = "http://freerangekitten.com/"
+    def test_ros_wiki(self):
+        url = "http://wiki.ros.org/"
         s = requests.Session()
         s.mount("http://", self.cls())
         r = s.get(url)
@@ -352,13 +257,6 @@ class TestForceAdapter(unittest.TestCase):
         with self.assertRaises(requests.exceptions.SSLError):
             s.get(url, timeout=5)
 
-    def test_my_vpnglobe(self):
-        url = "http://my.vpnglobe.com/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        with self.assertRaises(requests.exceptions.SSLError):
-            s.get(url)
-
     def _test_modwsgi_org(self):
         # https://github.com/EFForg/https-everywhere/issues/18867
         # http has a redirect to readthedocs; https fails
@@ -392,76 +290,15 @@ class TestForceAdapter(unittest.TestCase):
         original = r.history[0]
         self.assertEqual(original.url, url.replace("http://", "https://"))
 
-    def test_python_org_packages(self):
-        url = "http://packages.python.org/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        r = s.get(url)
-        r.raise_for_status()
-        self.assertEqual(r.url, "https://pythonhosted.org/")
-        self.assertEqual(len(r.history), 2)
-        original = r.history[0]
-        self.assertEqual(original.url, url)
-        self.assertEqual(original.status_code, 302)
-        self.assertEqual(r.history[1].url, url.replace("http://", "https://"))
-        self.assertEqual(r.history[1].status_code, 301)
 
-
-class TestPreferAdapter(unittest.TestCase):
+class TestPreferAdapter(TestForceAdapter):
 
     cls = PreferHTTPSAdapter
 
-    def test_freerangekitten_com(self):
-        url = "http://freerangekitten.com/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        r = s.get(url)
-        r.raise_for_status()
-        self.assertEqual(r.url, url.replace("http://", "https://"))
-        self.assertEqual(len(r.history), 1)
-        original = r.history[0]
-        self.assertEqual(original.url, url)
-
-    def test_example_com(self):
-        url = "http://example.com/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        r = s.get(url)
-        r.raise_for_status()
-        self.assertEqual(r.url, url.replace("http://", "https://"))
-        self.assertEqual(len(r.history), 1)
-        original = r.history[0]
-        self.assertEqual(original.url, url)
-
-    def test_webhostinggeeks_com_science(self):
-        # https://github.com/EFForg/https-everywhere/issues/18867
-        # https has wrong cert for quick failure.
-        url = "http://science.webhostinggeeks.com/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        with self.assertRaises(requests.exceptions.SSLError):
-            s.get(url)
-
-    def test_shopzilla(self):
-        url = "http://www.shopzilla.com/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        with self.assertRaises(requests.exceptions.TooManyRedirects):
-            s.get(url)
-
-    def test_whisper_sh(self):
-        url = "http://whisper.sh/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        with self.assertRaises(requests.exceptions.TooManyRedirects):
-            s.get(url)
-
-    def test_thesyriacampaign(self):
-        url = "http://www.thesyriacampaign.org/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        with self.assertRaises(requests.exceptions.TooManyRedirects):
-            s.get(url)
+    test_01_org = TestUpstreamAdapter.test_01_org
+    test_01_org_www = TestUpstreamAdapter.test_01_org
+    test_medbank_mt = TestUpstreamAdapter.test_medbank_mt
+    test_python_org_packages = TestUpstreamAdapter.test_python_org_packages
 
     def test_esncz_org(self):
         url = "http://www.isc.vutbr.cz/"
@@ -475,13 +312,6 @@ class TestPreferAdapter(unittest.TestCase):
         original = r.history[0]
         self.assertEqual(original.url, url)
         self.assertEqual(r.url, "https://www.esncz.org")
-
-    def test_my_vpnglobe(self):
-        url = "http://my.vpnglobe.com/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        with self.assertRaises(requests.exceptions.SSLError):
-            s.get(url)
 
     def _test_modwsgi_org(self):
         # https://github.com/EFForg/https-everywhere/issues/18867
@@ -498,49 +328,8 @@ class TestPreferAdapter(unittest.TestCase):
         self.assertEqual(len(r.history), 2)
         self.assertEqual(r.history[1].url, "https://modwsgi.readthedocs.io/")
 
-    def test_fedmsg_com(self):
-        # https://github.com/EFForg/https-everywhere/issues/18867
-        # https redirects to http, but there are no rules to upgrade it to https
-        url = "http://fedmsg.com/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        with self.assertRaises(requests.exceptions.TooManyRedirects):
-            s.get(url)
 
-        # It can be solved with an explicit manual exclusion, but cant
-        # be managed in an adapter because the TooManyRedirects is raised
-        # in the session
-        s.mount("http://", self.cls(https_exclusions=["fedmsg.com"]))
-        r = s.get(url)
-        r.raise_for_status()
-        self.assertEqual(r.url, url)
-        self.assertEqual(r.history, [])
-
-        s.mount("https://", self.cls(https_exclusions=["fedmsg.com"]))
-        r = s.get(url.replace("http://", "https://"))
-        r.raise_for_status()
-        self.assertEqual(r.url, url)
-        self.assertEqual(len(r.history), 1)
-        original = r.history[0]
-        self.assertEqual(original.url, url.replace("http://", "https://"))
-
-    def test_python_org_packages(self):
-        url = "http://packages.python.org/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        r = s.get(url)
-        r.raise_for_status()
-        self.assertEqual(r.url, "https://pythonhosted.org/")
-        self.assertEqual(len(r.history), 2)
-        original = r.history[0]
-        self.assertEqual(original.url, url)
-        # This adapter provides the real response, with code of 301
-        self.assertEqual(original.status_code, 301)
-        self.assertEqual(r.history[1].url, url.replace("http://", "https://"))
-        self.assertEqual(r.history[1].status_code, 301)
-
-
-class TestSafeUpgradeAdapter(unittest.TestCase):
+class TestSafeUpgradeAdapter(TestEverywhereAdapter):
 
     cls = SafeUpgradeHTTPSAdapter
 
@@ -562,55 +351,7 @@ class TestSafeUpgradeAdapter(unittest.TestCase):
         self.assertEqual(r.url, url.replace("http://", "https://"))
         self.assertEqual(r.history, [])
 
-    def test_webhostinggeeks_com_science(self):
-        # https://github.com/EFForg/https-everywhere/issues/18867
-        # https has wrong cert for quick failure.
-        url = "http://science.webhostinggeeks.com/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        r = s.get(url, timeout=5)
-        r.raise_for_status()
-        self.assertEqual(r.url, url)
-        self.assertEqual(r.history, [])
-
-    def test_shopzilla(self):
-        url = "http://www.shopzilla.com/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        r = s.get(url, timeout=5)
-        r.raise_for_status()
-        self.assertEqual(r.url, url)
-
-    def test_whisper_sh(self):
-        url = "http://whisper.sh/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        r = s.get(url, timeout=5)
-        r.raise_for_status()
-        self.assertEqual(r.url, url)
-
-    def test_thesyriacampaign(self):
-        url = "http://www.thesyriacampaign.org/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        r = s.get(url, timeout=5)
-        self.assertEqual(r.url, url)
-        # FIXME: this should redirect to 'https://thesyriacampaign.org/'
-        self.assertEqual(r.status_code, 403)
-
-    def test_esncz_org(self):
-        url = "http://www.isc.vutbr.cz/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-        r = s.get(url, timeout=5)
-        r.raise_for_status()
-        target = "https://www.esncz.org"
-        self.assertEqual(r.url, target)
-        self.assertEqual(len(r.history), 1)
-        original = r.history[0]
-        self.assertEqual(original.url, "http://www.isc.vutbr.cz/")
-
-    def test_my_vpnglobe(self):
+    def _test_my_vpnglobe(self):
         url = "http://my.vpnglobe.com/"
         s = requests.Session()
         s.mount("http://", self.cls())
@@ -633,25 +374,6 @@ class TestSafeUpgradeAdapter(unittest.TestCase):
         source_redirect = r.history[1]
         self.assertEqual(source_redirect.url, "https://modwsgi.readthedocs.io/")
 
-    def test_fedmsg_com(self):
-        # https://github.com/EFForg/https-everywhere/issues/18867
-        # https redirects to http, but there are no rules to upgrade it to https
-        url = "http://fedmsg.com/"
-        s = requests.Session()
-        s.mount("http://", self.cls())
-
-        r = s.get(url)
-        r.raise_for_status()
-        self.assertEqual(r.url, url)
-        self.assertEqual(r.history, [])
-
-        r = s.get(url.replace("http://", "https://"))
-        r.raise_for_status()
-        self.assertEqual(r.url, url)
-        self.assertEqual(len(r.history), 1)
-        original = r.history[0]
-        self.assertEqual(original.url, url.replace("http://", "https://"))
-
     def test_python_org_packages(self):
         url = "http://packages.python.org/"
         s = requests.Session()
@@ -664,3 +386,58 @@ class TestSafeUpgradeAdapter(unittest.TestCase):
         self.assertEqual(original.url, url.replace("http://", "https://"))
         # This adapter provides the real response, with code of 301
         self.assertEqual(original.status_code, 301)
+
+    def test_ros_wiki(self):
+        # https://github.com/jayvdb/pypidb/issues/115
+        # Short-lived problem
+        url = "http://wiki.ros.org/"
+        s = requests.Session()
+        s.mount("http://", self.cls())
+        r = s.get(url)
+        r.raise_for_status()
+        self.assertEqual(r.url, url.replace("http://", "https://"))
+        self.assertEqual(r.history, [])
+
+    def test_01_org(self):
+        url = "http://01.org/"
+        s = requests.Session()
+        s.mount("http://", self.cls())
+        r = s.get(url)
+        r.raise_for_status()
+        self.assertEqual(r.url, url.replace("http://", "https://"))
+        self.assertEqual(r.history, [])
+
+    def test_01_org_www(self):
+        url = "http://www.01.org/"
+        s = requests.Session()
+        s.mount("http://", self.cls())
+        r = s.get(url)
+        r.raise_for_status()
+        self.assertEqual(r.url, "https://01.org/")
+        self.assertEqual(len(r.history), 1)
+        original = r.history[0]
+        # Here is where it differs from upstream, with an unusual
+        # original url which differs from the real original
+        self.assertEqual(original.url, url.replace("http://", "https://"))
+        self.assertEqual(original.status_code, 301)
+        self.assertEqual(original.reason, "Moved Permanently")
+
+    def test_medbank_mt(self):
+        url = "http://business.medbank.com.mt/"
+        s = requests.Session()
+        s.mount("http://", self.cls())
+        r = s.get(url)
+        r.raise_for_status()
+        self.assertEqual(r.url, "https://www.medirect.com.mt")
+        self.assertEqual(len(r.history), 1)
+        original = r.history[0]
+        # Here is where it differs from upstream, with an unusual
+        # original url which differs from the real original
+        self.assertEqual(original.url, url.replace("http://", "https://"))
+        self.assertEqual(original.status_code, 301)
+        self.assertEqual(original.reason, "Moved Permanently")
+
+
+class TestUpgradeAdapter(TestForceAdapter):
+
+    cls = UpgradeHTTPSAdapter
